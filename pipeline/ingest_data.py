4e3915fa9194933318ff3abbe3f5f5ae65dc1fc1
Oluwaseyi Ogundimu <105808404+seyi-tkb@users.noreply.github.com>
2026-01-15 22:55:48 +0000

    pipeline's ingest_data script working

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,181 +1,110 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
+# import needed libraries
 import pandas as pd
-
-
-# In[3]:
+from sqlalchemy import create_engine    # database connector
+from tqdm.auto import tqdm              # helps monitor progress
 
 
 # confirm environment pandas is using/imported from.
 # our venv
-pd.__file__
+# pd.__file__
 
-
-# In[20]:
-
-
-# readability
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
-
-# Read a sample of the data
-df = pd.read_csv(url)
-
-
-# In[ ]:
-
-
-# Check data types
-df.dtypes
-
-
-# ###### To prevent type issue, clean data type on load as opposed to after.
-
-# In[23]:
-
-
-# clean data type on load
+# specify data type to ensure clean data type on load
 dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
+        "VendorID": "Int64",
+        "passenger_count": "Int64",
+        "trip_distance": "float64",
+        "RatecodeID": "Int64",
+        "store_and_fwd_flag": "string",
+        "PULocationID": "Int64",
+        "DOLocationID": "Int64",
+        "payment_type": "Int64",
+        "fare_amount": "float64",
+        "extra": "float64",
+        "mta_tax": "float64",
+        "tip_amount": "float64",
+        "tolls_amount": "float64",
+        "improvement_surcharge": "float64",
+        "total_amount": "float64",
+        "congestion_surcharge": "float64"
+        }
 
 # Parse dates
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-
-# In[29]:
-
-
-# Read a sample of the data
-df = pd.read_csv(url,
-                 dtype=dtype,
-                 parse_dates=parse_dates
-                )
-
-
-# In[30]:
-
-
-# Check data types
-df.dtypes
-
-
-# In[28]:
-
-
-# Check data shape
-df.shape
-
-
-# In[39]:
+parse_dates = ["tpep_pickup_datetime",
+               "tpep_dropoff_datetime"
+               ]
 
 
 # install connector to database
-get_ipython().system('uv add sqlalchemy')
-
-
-# In[40]:
-
+# get_ipython().system('uv add sqlalchemy')
 
 # we install binary version as that is faster
-get_ipython().system('uv add psycopg2-binary')
-
-
-# In[46]:
-
+# get_ipython().system('uv add psycopg2-binary')
 
 # install tqdm to help monitor progress
-get_ipython().system('uv add tqdm')
-
-
-# In[41]:
-
-
-# import our downloaded database connector
-from sqlalchemy import create_engine
-
-# create connection
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-
-# ###### Now we can use `df.sql` to insert into database
-
-# In[42]:
+# get_ipython().system('uv add tqdm')
 
 
 # how does this get the schema
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-
-# In[44]:
-
-
-# just create table with schema or column_names
-# it actually adds it to our dockerized database/postgres
-df.head(0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+# print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
 
 
 # ###### (MONITORED) INGESTION
 
-# In[51]:
+def run():
+    # to make it parameterizable
+    year = 2021
+    month = 1
+    # database credentials/parameters
+    pg_user = "root"
+    pg_pass = "root"
+    pg_host = "localhost"
+    pg_port = 5432
+    pg_db = "ny_taxi"
+    # other 
+    target_table = "yellow_taxi_data"
 
 
-# create iterators instead of dataframes
-df_iter = pd.read_csv(url,
-                      dtype=dtype,
-                      parse_dates=parse_dates,
-                      iterator= True,
-                      chunksize= 100000
-                )
+    # readability
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz' # pads month with 0, width - 2 characters
+
+    # create iterators instead of dataframes
+    df_iter = pd.read_csv(url,
+                        dtype=dtype,
+                        parse_dates=parse_dates,
+                        iterator= True,
+                        chunksize= 100000
+                    )
+
+    # create connection
+    engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
+
+    # actual ingestion script
+    first = True
+    for chunk_df in tqdm(df_iter):
+        
+        # just create table with schema or column_names
+            # it actually adds it to our dockerized database/postgres
+        if first:
+            chunk_df.head(0).to_sql(name=target_table, 
+                              con=engine, 
+                              if_exists='replace'
+                              )
+            print(f"Created table: {target_table}")
+            first = False
+        
+        # load and monitor in batches
+        chunk_df.to_sql(name=target_table, 
+                        con=engine, 
+                        if_exists='append'
+                        )
+
+        print("Inserted chunk:", len(chunk_df))
 
 
-# In[52]:
-
-
-# helps monitor progress
-from tqdm.auto import tqdm
-
-
-# In[53]:
-
-
-# actual ingestion script
-# load and monitor in batches
-for chunk_df in tqdm(df_iter):
-    chunk_df.to_sql(name='yellow_taxi_data', 
-                    con=engine, 
-                    if_exists='append')
-
-    print("Inserted chunk:", len(chunk_df))
-
-# what if i wanted to make it such that it doesn't ingest data it has loaded before in case of retries
-# basically one of ACID
-
-
-# In[ ]:
-
-
-
+if __name__ == '__main__':
+    run()
 
